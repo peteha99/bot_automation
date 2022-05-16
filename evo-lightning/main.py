4821afabe092ff5285e89e78005d891f6524d6d6
sarayut.sir <sarayut.siriwi@gmail.com>
2022-05-17 01:43:52 +0700

    add check waiting

--- evo-lightning/main.py
+++ evo-lightning/main.py
@@ -4,23 +4,24 @@ import time
 bot = Bot()
 
 print(bot.grab_img().getpixel(Coordinate().betStatRound))
-balance4 = 10000
-balance8 = 10000
-balance16 = 10000
-balance32 = 10000
-total4 = 0
-total8 = 0
-total16 = 0
-total32 = 0
-betStack4 = 0
-betStack8 = 0
-betStack16 = 0
-betStack32 = 0
+
 betPrice = 10
 while True:
     loseCount = 0
     loseStack = 0
     winner = 0
+    balance4 = 10000
+    balance8 = 10000
+    balance16 = 10000
+    balance32 = 10000
+    total4 = 0
+    total8 = 0
+    total16 = 0
+    total32 = 0
+    betStack4 = 0
+    betStack8 = 0
+    betStack16 = 0
+    betStack32 = 0
     while bot.grab_img().getpixel(Coordinate().greenBackground) == Status().connect:
         waitingRound = False
         betFinish = False
@@ -29,10 +30,7 @@ while True:
         print("in loop connect")
         while bot.grab_img().getpixel(Coordinate().betStatRound) == Status().redBetReady and bot.grab_img().getpixel(Coordinate().greenBackground) == Status().connect:
             print(loseStack)
-            print("total4",total4)
-            print("total8",total8)
-            print("total16",total16)
-            print("total32",total32)
+            
             time.sleep(3)
             if(bot.grab_img().getpixel(Coordinate().trackHistory) != Status().redWin and waitingRound == False):
                 loseCount = loseCount + 1
@@ -46,11 +44,18 @@ while True:
                 loseCount = 0
                 isContinueBet = True
             
-            if(loseCount == 0 and isContinueBet == False):
-                total4 = (betStack4 * 2) + balance4
-                total8 = (betStack8 * 2) + balance8
-                total16 = (betStack16 * 2) + balance16
-                total32 = (betStack32 * 2) + balance32 
+            if(loseCount == 0 and isContinueBet == False and calculate == False):
+                total4 = betStack4 * 2
+                total8 = betStack8 * 2
+                total16 = betStack16 * 2
+                total32 = betStack32 * 2
+                
+
+                print("total4",total4)
+                print("total8",total8)
+                print("total16",total16)
+                print("total32",total32)
+                calculate = True
             
             if(betFinish == False):
                 print("start bet")
@@ -62,6 +67,10 @@ while True:
                 total8 = balance8 - betStack8
                 total16 = balance16 - betStack16
                 total32 = balance32 - betStack32
+                print("total4",total4)
+                print("total8",total8)
+                print("total16",total16)
+                print("total32",total32)
                 betFinish = True
             
             
